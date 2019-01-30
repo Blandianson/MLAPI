8995027cf0927dd279c76407661299695ab68c98
Nicole Jackson <38741156+Blandianson@users.noreply.github.com>
2019-01-31 09:40:37 +1300

    Broken: File not releasing lock causing error right before presentation

--- MLAPI/Styles/MLAPI.js
+++ MLAPI/Styles/MLAPI.js
@@ -81,10 +81,9 @@ function collectData(inputArray, forecastArray) {
 }
 
 //Show Cleaned Data and/or Holiday Consideration
-function additionalOptions() {
+function additionalOptions(cleanArray) {
     var radio = document.getElementsByName("forecastRadio");
-
-    console.log(radio[0].checked + " " + radio[1].value)
+    
     for (var i = 0; i < radio.length; i++) {
 
         if (radio[i].checked && radio[i].value == "1") {
@@ -103,5 +102,5 @@ $(document).ready(function () {
     var allPoints = collectData(inputArray, forecastArray);
     initHighchart(allPoints, inputArray.length);
 
-    additionalOptions();
+    additionalOptions(cleanArray);
 });
